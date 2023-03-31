096a9d49be0ccf68e92dcc2985a70521d4a8492d
linmingye <lye@algoma.ca>
2023-03-31 18:41:18 -0400

    fix user redirect when save profile

--- src/Views/Profile.java
+++ src/Views/Profile.java
@@ -100,7 +100,12 @@ public class Profile extends Application {
 			UserService.setUserByEmail(Email, firstname, lastname, Pass, Role);
 
 			try {
-				new AdminUserDashBoardView().start(new Stage());
+				if (UserContext.getUser().isAdmin()) {
+					new AdminUserDashBoardView().start(new Stage());
+				} else {
+					new DashboardView().start(new Stage());
+				}
+
 			} catch (ClassNotFoundException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
